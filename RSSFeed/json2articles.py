30250238a96bed68cb21bea2fa8f63b88f63968e
Jon Fernquest <jonfernq@hawaii.edu>
2023-02-24 16:00:39 +0700

    Update json2articles.py

--- RSSFeed/json2articles.py
+++ RSSFeed/json2articles.py
@@ -16,3 +16,12 @@ for article in articles:
         out_file.write(f'Title: {title}\n')
         out_file.write(f'Published: {published}\n\n')
         out_file.write(summary)
+        
+"""
+This code assumes that the news articles are stored in a JSON file called news_articles.json. It reads in the JSON file and stores the list of articles in the articles variable.
+
+The code then iterates over each article in the list and extracts the title, published, and summary fields. It replaces any characters in the title that can't be used in a filename (e.g. /, \) and uses the title as the filename for the output text file.
+
+Finally, the code opens the output text file and writes out the title, publication date, and summary of the article in the specified format. Note that the output text file is encoded in UTF-8 to support non-ASCII characters.
+
+"""
